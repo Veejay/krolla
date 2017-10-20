95af5ce5b5474d903ea898c6558d59c49dc1157b
Bertrand Chardon <bertrand.chardon@gmail.com>
2017-10-21 01:19:43 +0200

    Implement naive worker pool

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const { Crawler } = require('./crawler.js')
 
-const crawler = new Crawler('https://fr.orson.io')
+const crawler = new Crawler({rootUrl: 'http://fr.orson.io', poolSize: 16})
 
 const main = async () => {
-  await crawler.crawl()
-  console.log('Done')
+  try {
+    await crawler.init()
+    const crawledUrls = await crawler.crawl()
+    console.log(`Crawled ${crawledUrls} URLs`)
+    console.log(`
+Number of links per URL:
+========================    
+    `)
+    console.log(crawler.count)
+    await crawler.browser.close()
+  } catch(e) {
+    console.error(e)
+  }
+  
 }
 
 main()
--- crawler.js
+++ crawler.js
@@ -1,18 +1,68 @@
 const URL = require('url')
 const puppeteer = require('puppeteer')
 const chalk = require('chalk')
+const fs = require('fs')
+class Worker {
+  // Kind of strange that the scouts themselves would grab the
+  // jobs but it'll for now
+  constructor(browser, crawler) {
+    this.browser = browser
+    this.crawler = crawler
+  }
+
+  async init(index) {
+    this.name = `worker_${index}`
+    console.log(`Initializing ${this.name}`)
+    return new Promise(async (resolve, reject) => {
+      this.page = await this.browser.newPage()
+      resolve(this)
+    })
+  }
+
+  async visit(url) {
+    console.log(`${chalk.bgBlue.white.bold("Visiting")} ${url}`)
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this.page.goto(url, {timeout: 20000})
+        const links = await this.page.evaluate(() => {
+          return Array.from(document.querySelectorAll('a')).map(link => link.href)
+        })
+        this.crawler.visitedUrls.add(url)
+        this.crawler.push(new Set(links))
+        resolve(true)
+      } catch(error) {
+        this.crawler.errors.add(url)
+        resolve(true)
+      }
+    })
+  }
+}
 
 class Crawler {
-  constructor(url) {
-    this.poolSize = 8
-    this.host = URL.parse(url).host
-    this.pendingUrls = new Set([url])
-    this.visitedUrls = new Map()
-    this.scouts = []
+  constructor({rootUrl, poolSize}) {
+    this.poolSize = poolSize
+    this.host = URL.parse(rootUrl).host
+    this.pendingUrls = new Set([rootUrl])
+    this.visitedUrls = new Set()
+    this.count = {}
+    this.errors = new Set()
   }
-  get report() {
-    const report = new Report(this.visitedLinks)
-    return report
+
+  async init() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.browser = await puppeteer.launch()
+        const workers = []
+        for (let i = 0; i < this.poolSize; i++) {
+          const worker = new Worker(this.browser, this)
+          workers.push(worker.init(i))
+        }
+        this.workers = await Promise.all(workers)
+        resolve(this)
+      } catch(e) {
+        reject(e)
+      }
+    })
   }
 
   popLocations() {
@@ -22,48 +72,24 @@ class Crawler {
   }
 
   async crawl() {
-    this.browser = await puppeteer.launch()
-    let targets = []
-    while (targets = this.popLocations()) {
-      console.log
-      const promises = targets.map(urls => {
-        const result = this.visitPage(urls)
-        console.log(result)
-        return result
-      })
-      await Promise.all(promises)
-    }
-  }
-
-  incrementVisits(url) {
-    if (this.visitedUrls.has(url)) {
-      const visits = this.visitedUrls.get(url)
-      this.visitedUrls.set(url, visits + 1)
-    } else {
-      this.visitedUrls.set(url, 1)
-    }
-  }
-
-  async visitPage(url) {
-    console.log(`Visiting ${url}`)
     return new Promise(async (resolve, reject) => {
-      try {
-        const page = await this.browser.newPage()
-        await page.goto(url)
-        const links = await page.evaluate(() => {
-          return Array.from(document.querySelectorAll('a')).map(link => link.href)
+      let locations = []
+      while (locations = this.popLocations()) {
+        if (locations.length === 0) {
+          break
+        }
+        const promises = locations.map((location, index) => {
+          return this.workers[index].visit(location)
         })
-        this.incrementVisits(url)
-        // FIXME: Makes no sense to push things to a crawler. The crawler should have a reference
-        // to a higher-order object controlling storage of and access to crawlable URLs
-        this.push(new Set(links))
-        resolve(true)
-      } catch(error) {
-        reject(error)
+        await Promise.all(promises)
       }
+      resolve(this.visitedUrls.size)
     })
     
+  }
 
+  incrementCount(url) {
+    this.count[url] = (this.count[url] || 0) + 1
   }
 
   outbound(url) {
@@ -71,7 +97,7 @@ class Crawler {
   }
 
   crawlable(url) {
-    return !(this.visitedUrls.has(url)) && !this.outbound(url)
+    return !this.visitedUrls.has(url) && !this.outbound(url)
   }
   /**
    * @description Adds potentiallly crawlable URLs to the queue
@@ -80,7 +106,10 @@ class Crawler {
    * @returns {void}
    */
   push(urls) {
-    for (let url of [...urls].filter(this.crawlable.bind(this))) {
+    for (let url of urls) {
+      this.incrementCount(url)
+    }
+    for (let url of [...urls].filter(url => {return this.crawlable(url) })) {
       this.pendingUrls.add(url)
     }
   }
